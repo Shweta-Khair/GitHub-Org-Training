3ff2f42bf566535cde6d87a1a9f818d82cb1c1b2
Shweta <shwetakhai@cybage.com>
2023-11-01 11:16:57 +0530

    Adding SRC folder

---
+++ src/main/java/com/jcg/maven/App.java
@@ -0,0 +1,15 @@
+package com.jcg.maven;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	
+class A{
+	private  String TEST ="";
+	
+}
+}
